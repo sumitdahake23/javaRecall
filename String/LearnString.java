df6265f1078d919124215da5e956f65e92a091b2
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2024-06-19 10:02:26 +0000

    String

---
+++ String/LearnString.java
@@ -0,0 +1,19 @@
+package String;
+
+public class LearnString {
+   public static void main(String[] args) {
+    String str="Sumit Dahake";
+    // Character Checking
+    System.out.println(str.charAt(4));
+    // o/p- t
+    // Size of a string
+    System.out.println(str.length());
+    // o/p- 12
+    System.out.println(str.substring(0,4));
+    // o/p- Sumi
+
+   // System.out.println(str.);
+
+
+   }
+}
